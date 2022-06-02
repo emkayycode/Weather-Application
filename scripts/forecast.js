e9ba07231d00cc15a66ef1d95e36cbea3e94146b
Ezinwanne Azubuike <101511317+emkayycode@users.noreply.github.com>
2022-06-02 02:06:34 +0100

    Add files via upload
    Real-Time weather application using JavaScript , Accuweather API integrated.
    Shows  time , day/night and real weather conditions .

---
+++ scripts/forecast.js
@@ -0,0 +1,31 @@
+
+const key = '	Rp30Rw4S3nbP36xN06icfrmUVq6ywY3c';
+
+// get weather information
+const getWeather = async (id) => {
+  
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+};
+
+// get city information
+const getCity = async (city) => {
+
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+};
+
+ 
+
